a9cac9565d1519ac9cdf994a75f1ee652cc8e56b
GTAManRCRX <gaaladrian1@gmail.com>
2026-02-12 12:27:50 +0100

    The imported assets' name display correctly

--- src/main/server/routes/asset.ts
+++ src/main/server/routes/asset.ts
@@ -275,16 +275,14 @@ group.route("POST", "/api/asset/upload", async (req, res) => {
 					}
 					if (ext == "webp" || ext == "tif" || ext == "avif") {
 						const image = await Jimp.read(filepath);
-        					const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
-        					stream = Readable.from(buffer);
 						var finalExt = "png";
 					} else {
-						stream = fs.createReadStream(filepath);
-						var finalExt = ext;
+						const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
+        				stream = Readable.from(buffer);
 					}
 					if (stream instanceof fs.ReadStream) {
         					stream.pause();
-    					}
+    				}
 					info.id = await AssetModel.save(stream, ext, info);
 				}
 				break;
